bd381f40246c28fc8a4c913bc7ec758deac91694
Anshu Kumar <116490760+Anshu-2801@users.noreply.github.com>
2025-06-23 08:32:08 +0000

    container config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId=12345;
+let accountEmail="anshuagrawal2812000@gmail.com";
+var accountPassword="123";
+accountCity="Gaya";
+
+// accountId=2;
+console.log(accountId);
+
+accountEmail="abs@gmail.com";
+accountPassword=222222;
+accountCity="patna";
+console.log([accountId,accountPassword,accountCity,accountEmail]);
